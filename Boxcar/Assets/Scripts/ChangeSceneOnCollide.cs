8359f92790b27d0945e1c61451291a6304b5090b
joncazier <joncazier@gmail.com>
2016-05-11 21:18:01 -0600

    Added Unity3d project

---
+++ Boxcar/Assets/Scripts/ChangeSceneOnCollide.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class ChangeSceneOnCollide : MonoBehaviour {
+
+	public string PortalToScene = "TestScene2";
+
+	void OnCollisionEnter2D(Collision2D collisionInfo)
+	{
+
+		if (collisionInfo.collider.name == "Bert") {
+			SceneManager.LoadScene (PortalToScene, mode: LoadSceneMode.Single);
+		}
+
+	}
+}
